4de36ecdb26fd30ffaf08d2c698aea27e7a8d3d2
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2018-03-06 20:32:43 +0100

    Create Xcode project

---
+++ TPPhotoEditor/TPPhotoEditor.h
@@ -0,0 +1,19 @@
+//
+//  TPPhotoEditor.h
+//  TPPhotoEditor
+//
+//  Created by Tomasz Pieczykolan on 06/03/2018.
+//  Copyright © 2018 Tomasz Pieczykolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TPPhotoEditor.
+FOUNDATION_EXPORT double TPPhotoEditorVersionNumber;
+
+//! Project version string for TPPhotoEditor.
+FOUNDATION_EXPORT const unsigned char TPPhotoEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TPPhotoEditor/PublicHeader.h>
+
+
